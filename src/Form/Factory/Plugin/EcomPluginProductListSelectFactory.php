6dc571fffe58ca31eb04c693c2dafa5adeacb5f1
Cris <crismakabenta@gmail.com>
2018-09-13 13:00:44 +0800

    Update select factory for related products

--- src/Form/Factory/Plugin/EcomPluginProductListSelectFactory.php
+++ src/Form/Factory/Plugin/EcomPluginProductListSelectFactory.php
@@ -11,6 +11,7 @@ namespace MelisCommerce\Form\Factory\Plugin;
 
 use MelisCore\Form\Factory\MelisSelectFactory;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\Session\Container;
 
 class EcomPluginProductListSelectFactory extends MelisSelectFactory
 {
@@ -21,11 +22,29 @@ class EcomPluginProductListSelectFactory extends MelisSelectFactory
         $serviceManager = $formElementManager->getServiceLocator();
 
         $productTble = $serviceManager->get('MelisEcomProductTable');
+        $container = new Container('meliscore');
+        $langIdBO = $container['melis-lang-id'];
+
         $productList = $productTble->getProductList(array(), null, null, null, null, 'prd_reference');
 
         foreach ($productList As $val)
         {
-            $valueoptions[$val->prd_id] = $val->prd_reference;
+            if(!empty($val->prd_reference)) {
+                $valueoptions[$val->prd_id] = $val->prd_reference;
+            }else{
+                /**
+                 * although its impossible to make the product reference
+                 * to be empty, but there are cases like if the database
+                 * is migrated and the product don't have a reference,
+                 * so if this will happen, we will used the product title
+                 */
+                $productText = $productTble->getProductText($val->prd_id, $langIdBO, 'TITLE')->toArray();
+                if(!empty($productText)){
+                    if(isset($productText[0])){
+                        $valueoptions[$val->prd_id] = $productText[0]['ptxt_field_short'];
+                    }
+                }
+            }
         }
 
         return $valueoptions;
