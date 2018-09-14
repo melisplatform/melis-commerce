c4f46714052f9d3766012fa46e7bef6721caf1e5
Franz Joseph Paran <fparan@melistechnology.com>
2018-09-14 12:46:24 +0800

    modified order messages dashboard plugin

--- src/Controller/DashboardPlugins/MelisCommerceDashboardPluginOrderMessages.php
+++ src/Controller/DashboardPlugins/MelisCommerceDashboardPluginOrderMessages.php
@@ -22,116 +22,94 @@ class MelisCommerceDashboardPluginOrderMessages extends MelisCoreDashboardTempla
         $this->pluginModule = 'meliscommerce';
         parent::__construct();
     }
-    /*
+
+    /**
      * Returns view for the Order Messages Dashboard Plugin
+     * @return ViewModel
      */
     public function orderMessages()
     {
         $view = new ViewModel();
         $view->setTemplate('MelisCommerceDashboardPluginOrderMessages/commerce-orders-messages');
+
         return $view;
     }
-    /*
-     * This controller gets all the messages from the client
-     * Returns JsonModel of messages or unanswered messages of the client
+
+    /**
+     * Gets all the messages from the client
+     * @return JsonModel
      */
     public function getMessages()
     {
         $success = 0;
         $messageList = array();
         $unansweredMessages = 0;
-        //check if post
-        if($this->getController()->getRequest()->isPost()) {
-            //get filter [data passed from the ajax]
-            $filter = get_object_vars($this->getController()->getRequest()->getPost());
-            $filter = isset($filter['filter']) ? $filter['filter'] : 'all';
-            //get Melis Commerce Order Service
-            $melisOrdersService = $this->getServiceLocator()->get('MelisComOrderService');
-            /*
-             * Initialize $noReply variable.
-             * This variable is used to check if the messages of the order
-             * is already seen by user of BO or the user of the BO has already replied
-             * to the order messages
-             */
+        $request = $this->getController()->getRequest();
+
+        if ($request->isPost()) {
+            $filter = $this->getFilter($request);
+            $orders = $this->getOrders();
             $noReply = true;
-            //get all orders
-            $orderDatas = $melisOrdersService->getOrderList(null, null, null, null, null, null, null, null, null, null, null, null, null);
 
-            foreach ($orderDatas as $order) {
-                //get all messages of order
-                $messages = $melisOrdersService->getOrderMessageByOrderId($order->getId());
-                //loop every message
-                foreach ($messages as $message){
-                    /*
-                     * If message osmg_user_id(BO user id who is talking) is NOT NULL
-                     * which means that the user who is talking is from the BO/Admin
-                     * we set the $noReply variable to false so that with even one message
-                     * we could detect if the admin had replied to the customer with their order
-                     */
-                    if($message->omsg_user_id != ""){
-                        $noReply = false;
-                    }
-                    //add the Customer Name for every message
-                    $message->clientFirstName = $order->getPerson()->cper_firstname;
-                    $message->clientLastName = $order->getPerson()->cper_name;
-                    //add order reference number because we will be needing it for the tabOpen helper
-                    //to name the zone
-                    $message->reference = $order->getOrder()->ord_reference;
-                }
+            foreach ($orders as $order) {
+                $messages = $order->getMessages();
 
-                //if we want all messages
-                if ($filter == 'all')
-                {
-                    //check if the order has messages
-                    if(count($order->getMessages()) > 0){
-                        foreach ($messages as $message){
-                            // f NO BO user has replied to the order
-                            if($noReply)
-                            {
-                                $unansweredMessages++;
-                                $message->noReply = true;
-                            }
-                            else{
-                                $message->noReply = false;
-                            }
-                            //push the client message to the list of meesages to send to the ajax
-                            if($message->omsg_user_id == "")
-                            array_push($messageList, $message);
+                if (count($messages) > 0) {
+
+                    foreach ($messages as $message) {
+                        if ($message->omsg_user_id != "") {
+                            $noReply = false;
                         }
+
+                        // get client name and reference number
+                        $message->clientFirstName = $order->getPerson()->cper_firstname;
+                        $message->clientLastName = $order->getPerson()->cper_name;
+                        $message->reference = $order->getOrder()->ord_reference;
+                        $message->orderDate = $this->formatDateByLangLocale($order->getOrder()->ord_date_creation);
+                        $message->totalOrderAmount = number_format($this->getTotalOrderAmount($order), 2);
                     }
-                }
-                //if we only wnat the unseen / unanswered messages
-                else if ($filter == 'unseen')
-                {
-                    //check if the current order has NOT been replied by the BO user
-                    if($noReply == true) {
-                        //because we set the $noReply variable to true
-                        //we still have to check if the order has messages
-                        if(count($order->getMessages()) > 0){
+
+                    if ($filter == 'all') {
+                        if (count($messages) > 0) {
                             foreach ($messages as $message) {
-                                //if NO BO user has replied to the order
-                                if($noReply)
-                                {
+                                // If no BO user has replied to the order
+                                $message->noReply = $noReply;
+
+                                if ($noReply) {
                                     $unansweredMessages++;
-                                    $message->noReply = true;
                                 }
-                                else{
-                                    $message->noReply = false;
+
+                                if ($message->omsg_user_id == "") {
+                                    array_push($messageList, $message);
                                 }
-                                //push the client unanswered message to the list of meesages to send to the ajax
-                                if($message->omsg_user_id == "")
-                                array_push($messageList, $message);
                             }
                         }
+                    } else if ($filter == 'unseen') {
+                        if ($noReply) {
+                            if (count($messages) > 0) {
+                                foreach ($messages as $message) {
+                                    //if NO BO user has replied to the order
+                                    $message->noReply = $noReply;
+
+                                    if ($noReply) {
+                                        $unansweredMessages++;
+                                    }
+
+                                    if ($message->omsg_user_id == "") {
+                                        array_push($messageList, $message);
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+
                     }
-                } else {
 
+                    //sort all messages by date_creation in descending order to show the latest message on the top
+                    array_multisort(array_column($messageList, "omsg_date_creation"), SORT_DESC, $messageList);
+                    // we set the $noReply variable to true again for the next order
+                    $noReply = true;
                 }
-
-                //sort all messages by date_creation in descending order to show the latest message on the top
-                array_multisort(array_column($messageList, "omsg_date_creation"), SORT_DESC, $messageList);
-                // we set the $noReply variable to true again for the next order
-                $noReply = true;
             }
         }
 
@@ -140,5 +118,62 @@ class MelisCommerceDashboardPluginOrderMessages extends MelisCoreDashboardTempla
             'messages' => $messageList,
             'unansweredMessages' => $unansweredMessages,
         ));
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLangLocale()
+    {
+        $container = new Container('meliscore');
+        $langLocale = $container['melis-lang-locale'];
+
+        return $langLocale;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOrders()
+    {
+        $melisOrdersService = $this->getServiceLocator()->get('MelisComOrderService');
+        $orders = $melisOrdersService->getOrderList(null, null, null, null, null, null, null, null, null, null, null, null, null);
+
+        return $orders;
+    }
+
+    /**
+     * @param $date
+     * @return string
+     */
+    public function formatDateByLangLocale($date)
+    {
+        $melisTranslation = $this->getServiceLocator()->get('MelisCoreTranslation');
+        $langLocale = $this->getLangLocale();
+        $formattedDate = strftime($melisTranslation->getDateFormatByLocate($langLocale), strtotime($date));
+
+        return $formattedDate;
+    }
+
+    public function getFilter($request)
+    {
+        $filter = get_object_vars($request->getPost());
+
+        if (isset($filter['filter'])) {
+            return $filter['filter'];
+        } else {
+            return 'all';
+        }
+    }
+
+    public function getTotalOrderAmount($order)
+    {
+        $totalOrderAmount = 0;
+
+        foreach ($order->getPayment() as $payment) {
+            $totalOrderAmount = +$payment->opay_price_total;
+        }
+
+        return $totalOrderAmount;
     }
 }
