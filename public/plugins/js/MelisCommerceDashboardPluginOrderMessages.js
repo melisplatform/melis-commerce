711681bf2c03b57f5777f9b06f54000a6c4f8914
Franz Joseph Paran <fparan@melistechnology.com>
2018-10-05 18:29:22 +0800

    [Issue #0002664] Could now update messages without refreshing every minute.

--- public/plugins/js/MelisCommerceDashboardPluginOrderMessages.js
+++ public/plugins/js/MelisCommerceDashboardPluginOrderMessages.js
@@ -1,76 +1,25 @@
-$(document).ready(function() {
+var commerceDashPluginOrderMessagesAppendMessagesInterval = '';
+var commerceDashPluginorderMessagesInstanceCount = 0;
 
-    //when filtering the messages
+$(document).ready(function() {
+    var filter = '';
+    var placeholder = '';
+    var messagecountplaceholder = '';
+    var intervalDelay = 60000;
+
+    // changing filter for charts
     $("body").on("change", '.commerce-dashboard-plugin-order-messages', function() {
         commerceDashboardPluginOrderMessagesInit($(this));
     });
 
+    //opening the message in orders
     $("body").on("click", ".commerce-dashboard-plugin-order-messages", function(){
-        var orderId = $(this).find('.order-message-id').val();
-        var orderReference = $(this).find('.order-message-reference').val();
-
-        // Open parent tab
-        melisHelper.tabOpen(
-            translations.tr_meliscommerce_orders_Orders,
-            'fa fa fa-cart-plus fa-2x',
-            'id_meliscommerce_order_list_page',
-            'meliscommerce_order_list_page'
-        );
-
-        var alreadyOpen = $("body #melis-id-nav-bar-tabs li a.tab-element[data-id='id_meliscommerce_order_list_page']");
-        // check if it exists
-        var checkOrders = setInterval(function() {
-            if (alreadyOpen.length) {
-                var navTabsGroup = "id_meliscommerce_order_list_page";
-
-                melisHelper.tabOpen(
-                    translations.tr_meliscommerce_orders_Order+' '+orderReference,
-                    'fa fa fa-cart-plus fa-2x',
-                    orderId+'_id_meliscommerce_orders_page',
-                    'meliscommerce_orders_page',
-                    { orderId : orderId},
-                    navTabsGroup,
-                    function(){
-                    //JS CALLBACK FOR THE ORDER MESSAGES
-                    //TO OPEN THE MESSAGE TAB OF A SPECIFIC ORDER
-                    var parent = orderId + '_id_meliscommerce_orders_content_tabs';
-
-                    $('#' + parent).find('.widget-head').find('ul').find('li').each(function(){
-                        if ($(this).find('a').attr('href') == "#" + orderId + "_id_meliscommerce_orders_content_tabs_content_messages") {
-                            $(this).addClass("active");
-                        } else {
-                            if($(this).hasClass("active")) {
-                                $(this).removeClass("active");
-                            }
-                        }
-                    });
-
-                    //get parent for the content
-                    var parent = orderId + '_id_meliscommerce_orders_content_tabs_content';
-
-                    $('#'+parent).find('.tab-pane').each( function(){
-                       if ($(this).attr('id') == orderId + "_id_meliscommerce_orders_content_tabs_content_messages") {
-                           $(this).addClass("active");
-                       } else {
-                           if ($(this).hasClass("active")) {
-                               $(this).removeClass("active");
-                           }
-                       }
-                    });
-                });
-                clearInterval(checkOrders);
-            }
-        }, 500);
+        orderMessages.openOrderMessages(this);
     });
 
-
     window.commerceDashboardPluginOrderMessagesInit = function(target){
-        var filter = null;
-        var placeholder = "";
-        var messagecountplaceholder = "";
-
         if (typeof target === "undefined") {
-            //first load
+            //first load or when using "all" filter
             filter = 'all';
             if (melisDashBoardDragnDrop.getCurrentPlugin() == null) {
                 placeholder = '.commerce-dashboard-plugin-order-messages-list';
@@ -80,12 +29,23 @@ $(document).ready(function() {
                 messagecountplaceholder = '#'+melisDashBoardDragnDrop.getCurrentPlugin().find(".message-count").attr("id");
             }
         } else {
-            //get clicked button value
+            //when "unanswered" filter is used
             filter = target.val();
             placeholder = "#"+target.closest(".melis-commerce-dashboard-plugin-order-messages-parent").find(".commerce-dashboard-plugin-order-messages-list").attr("id");
             messagecountplaceholder = "#"+target.closest('.commerce-dashboard-plugin-messages-head').find('.message-count').attr('id');
         }
 
+        commerceDashPluginorderMessagesInstanceCount = $('.commerce-dashboard-plugin-order-messages-list').length;
+        appendMessages();
+        appendMessagesInterval = setInterval(appendMessages, intervalDelay);
+    }
+
+    //initialize the order messages that are already in the dashboard
+    if ($('.commerce-dashboard-plugin-order-messages-list').length > 0) {
+        commerceDashboardPluginOrderMessagesInit();
+    }
+
+    function appendMessages() {
         $.ajax({
             type        : 'POST',
             url         : '/melis/dashboard-plugin/MelisCommerceDashboardPluginOrderMessages/getMessages',
@@ -94,89 +54,147 @@ $(document).ready(function() {
             encode		: true
         }).success(function(data) {
             //empty divs first
-            $(placeholder).empty();
-            $(messagecountplaceholder).empty();
-
-            if (data.unansweredMessages > 1) {
-                var message = translations.tr_melis_commerce_dashboard_plugin_order_messages_unanswered_messages;
-                //replace x with the count of unanswered messages
-                var newMessage = message.replace("%d", data.unansweredMessages);
-                $(messagecountplaceholder).append(newMessage);
-            } else {
-                var message = translations.tr_melis_commerce_dashboard_plugin_order_messages_unanswered_messages;
-                //replace x with the count of unanswered messages
-                var newMessage = message.replace("messages", "message").replace("%d", data.unansweredMessages);
-                $(messagecountplaceholder).append(newMessage);
-            }
+            orderMessages.clear(placeholder, messagecountplaceholder);
+            orderMessages.setUnansweredMessages(data.unansweredMessages);
 
             $.each(data.messages, function(index, message){
-                if (message.omsg_message.length > 70) {
-                    var text = message.omsg_message.substring(0, 70) + '...';
-                } else {
-                    var text = message.omsg_message;
-                }
-
-                var message_created = moment(message.omsg_date_creation, "YYYY-MM-DD HH:mm:ss");
-
-                if (message.noReply) {
-                    var dateHtml = '<span class="label label-inverse pull-right" style="background-color: #981a1f;">' +
-                        message_created.format("HH:mm:ss DD MMM") +
-                        '</span>';
-
-                    var doubleArrow = '<i class="fa fa-angle-double-right" style="color: #981a1f;"<i/>';
-
-                    var nameHtml =  '<span class="strong" style="color: #981a1f;">' +
-                        message.clientFirstName + ' ' + message.clientLastName +
-                        '</span> ' + doubleArrow +
-                        ' <small class="strong" style="color:#981a1f;">' +
-                        translations.tr_melis_commerce_dashboard_plugin_order_messages_message_order_amount + message.totalOrderAmount +
-                        '</small> ' + doubleArrow +
-                        ' <small class="strong" style="color:#981a1f;">' +
-                        translations.tr_melis_commerce_dashboard_plugin_order_messages_message_placed_on + message.orderDate +
-                        '</small>';
-
-                } else {
-                    var dateHtml = '<span class="label label-inverse label-stroke pull-right">' +
-                        message_created.format("HH:mm:ss DD MMM") +
-                        '</span>';
-
-                    var doubleArrow = '<i class="fa fa-angle-double-right"<i/>';
-
-                    var nameHtml =  '<span>' +
-                        message.clientFirstName + ' ' + message.clientLastName +
-                        '</span> ' + doubleArrow +
-                        ' <small>' +
-                        translations.tr_melis_commerce_dashboard_plugin_order_messages_message_order_amount + message.totalOrderAmount +
-                        '</small> ' + doubleArrow +
-                        ' <small>' +
-                        translations.tr_melis_commerce_dashboard_plugin_order_messages_message_placed_on + message.orderDate +
-                        '</small>';
-                }
-
-                var html =  '<a href="#" class="list-group-item commerce-dashboard-plugin-order-messages" ' +
-                    'style="border-radius: 0px;border-top: 0px;border-right: 0px;border-left:0px;margin-bottom: 0px;">' +
-                    '    <input class="order-message-id" type="text" value="' + message.omsg_order_id + '" hidden="hidden">' +
-                    '    <input class="order-message-reference" type="text" value="' + message.reference + '" hidden="hidden">' +
-                    '    <span class="media">' +
-                    '        <span class="media-body media-body-inline">' +
-                    dateHtml +
-                    nameHtml +
-                    '            <p class="list-group-item-text" style="font-size:12px;"> ' + text + ' ' +
-                    '            </p>' +
-                    '        </span>' +
-                    '    </span>' +
-                    '</a>';
-
-                //append the message
-                $(placeholder).append(html);
+                orderMessages.setMessages(placeholder, message);
             });
         }).error(function(xhr, textStatus, errorThrown) {
             console.log("ERROR !! Status = "+ textStatus + "\n Error = "+ errorThrown + "\n xhr = "+ xhr.statusText);
         });
     }
 
-    $('.commerce-dashboard-plugin-order-messages-list').each(function(){
-        commerceDashboardPluginOrderMessagesInit();
-    });
+    var orderMessages = {
+        openOrderMessages: function(element) {
+            var orderId = $(element).find('.order-message-id').val();
+            var orderReference = $(element).find('.order-message-reference').val();
 
+            // Open parent tab
+            melisHelper.tabOpen(
+                translations.tr_meliscommerce_orders_Orders,
+                'fa fa fa-cart-plus fa-2x',
+                'id_meliscommerce_order_list_page',
+                'meliscommerce_order_list_page'
+            );
+
+            var alreadyOpen = $("body #melis-id-nav-bar-tabs li a.tab-element[data-id='id_meliscommerce_order_list_page']");
+            // check if it exists
+            var checkOrders = setInterval(function() {
+                if (alreadyOpen.length) {
+                    var navTabsGroup = "id_meliscommerce_order_list_page";
+
+                    melisHelper.tabOpen(
+                        translations.tr_meliscommerce_orders_Order+' '+orderReference,
+                        'fa fa fa-cart-plus fa-2x',
+                        orderId+'_id_meliscommerce_orders_page',
+                        'meliscommerce_orders_page',
+                        { orderId : orderId},
+                        navTabsGroup,
+                        function() {
+                            //JS CALLBACK FOR THE ORDER MESSAGES
+                            //TO OPEN THE MESSAGE TAB OF A SPECIFIC ORDER
+                            var parent = orderId + '_id_meliscommerce_orders_content_tabs';
+
+                            $('#' + parent).find('.widget-head').find('ul').find('li').each(function(){
+                                if ($(this).find('a').attr('href') == "#" + orderId + "_id_meliscommerce_orders_content_tabs_content_messages") {
+                                    $(this).addClass("active");
+                                } else {
+                                    if($(this).hasClass("active")) {
+                                        $(this).removeClass("active");
+                                    }
+                                }
+                            });
+
+                            var parent = orderId + '_id_meliscommerce_orders_content_tabs_content';
+
+                            $('#'+parent).find('.tab-pane').each( function(){
+                                if ($(this).attr('id') == orderId + "_id_meliscommerce_orders_content_tabs_content_messages") {
+                                    $(this).addClass("active");
+                                } else {
+                                    if ($(this).hasClass("active")) {
+                                        $(this).removeClass("active");
+                                    }
+                                }
+                            });
+                        });
+                    clearInterval(checkOrders);
+                }
+            }, 500);
+        },
+        clear: function(placeholder, messagecountplaceholder) {
+            $(placeholder).empty();
+            $(messagecountplaceholder).empty();
+        },
+        setUnansweredMessages: function(unansweredMessages) {
+            var message = '';
+            var newMessage = '';
+
+            if (unansweredMessages > 1) {
+                message = translations.tr_melis_commerce_dashboard_plugin_order_messages_unanswered_messages;
+                newMessage = message.replace("%d", unansweredMessages);
+                $(messagecountplaceholder).append(newMessage);
+            } else {
+                message = translations.tr_melis_commerce_dashboard_plugin_order_messages_unanswered_messages;
+                newMessage = message.replace("messages", "message").replace("%d", unansweredMessages);
+                $(messagecountplaceholder).append(newMessage);
+            }
+        },
+        setMessages: function(placeholder, message) {
+            var colorRed = '';
+            var classStrong = '';
+            var text = message.omsg_message.length > 70 ? message.omsg_message.substring(0, 70) + '...' : message.omsg_message;
+            var message_created = moment(message.omsg_date_creation, "YYYY-MM-DD HH:mm:ss");
+
+            if (message.noReply) {
+                colorRed = 'style="background-color: #981a1f;"';
+                classStrong = 'strong';
+            }
+
+            var dateHtml = '<span class="label label-inverse pull-right" ' + colorRed + '>' +
+                                message_created.format("HH:mm:ss DD MMM") +
+                            '</span>';
+
+            var doubleArrow = '<i class="fa fa-angle-double-right" ' + colorRed + '<i/>';
+
+            var nameHtml =  '<span class="' + classStrong + '" ' + colorRed + '>' +
+                                message.clientFirstName + ' ' + message.clientLastName +
+                            '</span> ' + doubleArrow +
+                            ' <small class="' + classStrong + '" ' + colorRed + '>' +
+                                translations.tr_melis_commerce_dashboard_plugin_order_messages_message_order_amount + message.totalOrderAmount +
+                            '</small> ' + doubleArrow +
+                            ' <small class="' + classStrong + '" ' + colorRed + '>' +
+                                translations.tr_melis_commerce_dashboard_plugin_order_messages_message_placed_on + message.orderDate +
+                            '</small>';
+
+            var messageHtml =  '<a href="#" class="list-group-item commerce-dashboard-plugin-order-messages" ' +
+                                'style="border-radius: 0px;border-top: 0px;border-right: 0px;border-left:0px;margin-bottom: 0px;">' +
+                                '  <input class="order-message-id" type="text" value="' + message.omsg_order_id + '" hidden="hidden">' +
+                                '  <input class="order-message-reference" type="text" value="' + message.reference + '" hidden="hidden">' +
+                                '  <span class="media">' +
+                                '    <span class="media-body media-body-inline">' +
+                                     dateHtml +
+                                     nameHtml +
+                                '    <p class="list-group-item-text" style="font-size:12px;"> ' +
+                                      text + ' ' +
+                                '    </p>' +
+                                '    </span>' +
+                                '  </span>' +
+                                '</a>';
+
+            //append the message
+            $(placeholder).append(messageHtml);
+        }
+    };
 });
+
+//delete callback
+function commerceDasboardPluginOrderMessagesDelete(element) {
+    if (element.find(".melis-commerce-dashboard-plugin-order-messages-parent").length == 1) {
+        commerceDashPluginorderMessagesInstanceCount--;
+        if (commerceDashPluginorderMessagesInstanceCount == 0) {
+            console.log("clearing interval for order messages");
+            clearInterval(appendMessagesInterval);
+        }
+    }
+}
