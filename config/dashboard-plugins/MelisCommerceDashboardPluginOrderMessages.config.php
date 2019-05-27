8741181d473d62eecaf8629d8ff80e39795bac37
Cris <crismakabenta@gmail.com>
2019-05-27 16:28:20 +0800

    Adde section on commerce plugins datas

--- config/dashboard-plugins/MelisCommerceDashboardPluginOrderMessages.config.php
+++ config/dashboard-plugins/MelisCommerceDashboardPluginOrderMessages.config.php
@@ -49,6 +49,19 @@ return array(
                         'x-axis' => 0,
                         'y-axis' => 0,
                         'deleteCallback' => 'commerceDasboardPluginOrderMessagesDelete',
+                        /*
+                         * if set this plugin will belong to a specific marketplace section,
+                           * if not it will go directly to ( Others ) section
+                           *  - available section for dashboard plugins as of 2019-05-16
+                           *    - MelisCore
+                           *    - MelisCms
+                           *    - MelisMarketing
+                           *    - MelisSite
+                           *    - MelisCommerce
+                           *    - Others
+                           *    - CustomProjects
+                         */
+                        'section' => 'MelisCommerce',
                     ),
                     'forward' => array(
                         'module' => 'MelisCommerce',
--- config/dashboard-plugins/MelisCommerceDashboardPluginSalesRevenue.config.php
+++ config/dashboard-plugins/MelisCommerceDashboardPluginSalesRevenue.config.php
@@ -49,6 +49,19 @@
                             'x-axis' => 0,
                             'y-axis' => 0,
                             'activeFilter' => 'hourly',
+                            /*
+                             * if set this plugin will belong to a specific marketplace section,
+                               * if not it will go directly to ( Others ) section
+                               *  - available section for dashboard plugins as of 2019-05-16
+                               *    - MelisCore
+                               *    - MelisCms
+                               *    - MelisMarketing
+                               *    - MelisSite
+                               *    - MelisCommerce
+                               *    - Others
+                               *    - CustomProjects
+                             */
+                            'section' => 'MelisCommerce',
                         ),
                         'forward' => array(
                             'module' => 'MelisCommerce',
