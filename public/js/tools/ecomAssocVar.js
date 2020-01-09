bcaea9b9862fc5b94f61cbaee0525efb47e706f8
Junry Zabate <jzabate@melistechnology.com>
2020-01-09 15:31:27 +0800

    dataTables responsive mobile view before button

--- public/js/tools/ecomAssocVar.js
+++ public/js/tools/ecomAssocVar.js
@@ -170,14 +170,14 @@ $(function() {
             var $this               = $(this),
                 parentTable         = $this.parents('.tableAssocVariantList2'),
                 tableId             = parentTable.attr("id"),
-                tableInstance       = $(tableId), //eval("$"+tableId) ,
+                tableInstance       = $(tableId).DataTable(), //eval("$"+tableId) ,
                 currentVariantId    = parentTable.data("variantid"),
                 tr                  = $this.closest('tr'),
-                //row                 = tableInstance.row( tr ),            
+                row                 = tableInstance.row( tr ),            
                 productId           = tr.attr("id"); // Getting the product Id from the row Id
 
                 console.log({tableId});
-                console.log("eval($tableId): ", eval("$"+tableId));
+                console.log("tableInstance: ", tableInstance);
             
                 if ( row.child.isShown() ) {
                     // This row is already open - close it
