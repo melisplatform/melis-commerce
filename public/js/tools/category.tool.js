cff9f7c9af3348d05c9c23b0114ffbf194005569
Junry Zabate <jzabate@melistechnology.com>
2020-02-13 17:03:00 +0800

    fixing issue dev1 - Catalogues - products - tooltip error mantis uat #0010277

--- public/js/tools/category.tool.js
+++ public/js/tools/category.tool.js
@@ -523,7 +523,10 @@ $(function() {
 		});
 
 		$body.on("click", "#categoryInfoPanel .widget-head .nav-tabs li a.shopping_cart", function() {
-			$("#id_meliscommerce_categories_category_tab_products").find(".categoryProductsRefresh").trigger("click");
+			// trigger refresh on of the tab
+			if ( melisCore.screenSize <= 767 ) {
+				$("#id_meliscommerce_categories_category_tab_products").find(".categoryProductsRefresh").trigger("click");
+			}
 		});
 });
 
