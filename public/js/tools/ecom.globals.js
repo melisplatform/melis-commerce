1ec1e2a7c3e89e7c70382143c73a524355454f57
Cris <crismakabenta@gmail.com>
2024-07-12 17:54:40 +0800

    Change comments to asterisk: causing problem on bundling all

--- public/js/tools/ecom.globals.js
+++ public/js/tools/ecom.globals.js
@@ -1,4 +1,4 @@
-var contactToolSelectedAccount = '';//holds the selected account when linking/unlinking
-var accountToolSelectedContact = '';//holds the selected contact when linking/unlinking
-var contactToolInitAccountAutoSuggest = false;//use to check whether we need to re init the auto suggest of account tool when linking/unliking
-var accountToolInitContactAutoSuggest = false;//use to check whether we need to re init the auto suggest of contact tool when linking/unliking
+var contactToolSelectedAccount = '';/*holds the selected account when linking/unlinking*/
+var accountToolSelectedContact = '';/*holds the selected contact when linking/unlinking*/
+var contactToolInitAccountAutoSuggest = false;/*use to check whether we need to re init the auto suggest of account tool when linking/unliking*/
+var accountToolInitContactAutoSuggest = false;/*use to check whether we need to re init the auto suggest of contact tool when linking/unliking*/
