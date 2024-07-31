79eef933ca7c455e6a853db754211245ef174667
jzabate <jzabate@melistechnology.com>
2024-07-31 13:50:01 +0800

    rebundle removed console logs

--- public/js/tools/coupon.tool.js
+++ public/js/tools/coupon.tool.js
@@ -514,11 +514,8 @@ $(function() {
 				$(href).find(".couponClientListRefresh").trigger("click");
 
 			var clientListTimeout = setTimeout(function() {
-				console.log(`setTimeout 2000 $("#"+couponId+"_clientList").length: `, $("#"+couponId+"_clientList").length );
-				console.log(`setTimeout 2000 $("#"+couponId+"_clientList tbody tr").length > 1: `, $("#"+couponId+"_clientList tbody tr").length > 1 );
 				if ( $("#"+couponId+"_clientList").length && $("#"+couponId+"_clientList tbody tr").length > 1 ) {				
 					$("#"+couponId+"_clientList tbody tr td:first-child").addClass("dtr-control");
-					console.log(`dtr-control supposedly added!!!`);
 					clearTimeout( clientListTimeout );
 				}
 			}, 2000);
