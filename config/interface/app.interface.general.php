3bf69106587f749b53fef1d5a6cb3c5384e9ac2f
Maria Teresa Pomar <mpomar@melistechnology.com>
2026-07-16 16:37:56 +0800

    Revert "fix(rights): align commerce toolstree node-keys to their melisKeys"
    This reverts commit e670e21c99c836c0175d4b9187081563d913782f.

--- config/interface/app.interface.general.php
+++ config/interface/app.interface.general.php
@@ -32,68 +32,68 @@ return [
                                 'rights_checkbox_disable' => false,
                             ],
                             'interface' => [
-                                'meliscommerce_categories_page' => [
+                                'meliscommerce_categories' => [
                                     'conf' => [
                                         'type' => 'meliscommerce/interface/meliscommerce_categories/interface/meliscommerce_categories_leftmenu'
                                     ],
                                 ],
-                                'meliscommerce_product_list_container' => [
+                                'meliscommerce_product_list' => [
                                     'conf' => [
                                         'type' => 'meliscommerce/interface/meliscommerce_product_list/interface/meliscommerce_product_list_leftmenu'
                                     ],
                                 ],
-                                'meliscommerce_clients_list_page' => [
+                                'meliscommerce_clients_list' => [
                                     'conf' => [
                                         'type' => 'meliscommerce/interface/meliscommerce_clients_list/interface/meliscommerce_clients_list_leftmenu'
                                     ],
                                 ],
-                                'meliscommerce_contact_list_page' => [
+                                'meliscommerce_contact_list' => [
                                     'conf' => [
                                         'type' => 'meliscommerce/interface/meliscommerce_contact_list/interface/meliscommerce_contact_list_leftmenu'
                                     ],
                                 ],
-                                'meliscommerce_order_list_page' => [
+                                'meliscommerce_order_list' => [
                                     'conf' => [
                                         'type' => 'meliscommerce/interface/meliscommerce_order_list/interface/meliscommerce_order_list_leftmenu'
                                     ],
                                 ],
-                                'meliscommerce_coupon_list_page' => [
+                                'meliscommerce_coupon_list' => [
                                     'conf' => [
                                         'type' => 'meliscommerce/interface/meliscommerce_coupon_list/interface/meliscommerce_coupon_list_leftmenu'
                                     ],
                                 ],
-                                'meliscommerce_attribute_list_page' => [
+                                'meliscommerce_attribute_list' => [
                                     'conf' => [
                                         'type' => 'meliscommerce/interface/meliscommerce_attribute_list/interface/meliscommerce_attribute_list_leftmenu'
                                     ],
                                 ],
-                                'meliscommerce_country_list_container' => [
+                                'meliscommerce_country_list' => [
                                     'conf' => [
                                         'type' => 'meliscommerce/interface/meliscommerce_country_list/interface/meliscommerce_country_list_leftmenu'
                                     ],
                                 ],
-                                'meliscommerce_language_list_container' => [
+                                'meliscommerce_language_list' => [
                                     'conf' => [
                                         'type' => 'meliscommerce/interface/meliscommerce_language_list/interface/meliscommerce_language_list_leftmenu'
                                     ],
                                 ],
-                                'meliscommerce_currency_conf' => [
+                                'meliscommerce_currency_lists' => [
                                     'conf' => [
                                         'type' => 'meliscommerce/interface/meliscommerce_currency_lists/interface/meliscommerce_currency_left_menu'
                                     ],
                                 ],
-                                'meliscommerce_order_status_tool_page' => [
+                                'meliscommerce_order_status_lists' => [
                                     'conf' => [
                                         'type' => 'meliscommerce/interface/meliscommerce_order_status_tool/interface/meliscommerce_order_status_tool_leftmenu'
                                     ],
                                 ],
-                                'meliscommerce_clients_group_tool_container' => [
+                                'meliscommerce_clients_group_list' => [
                                     'conf' => [
                                         'type' => 'meliscommerce/interface/meliscommerce_clients_group_tool/interface/meliscommerce_clients_group_tool_leftmenu'
                                     ],
                                 ],
 
-                                'meliscommerce_settings_page' => [
+                                'meliscommerce_settings' => [
                                     'conf' => [
                                         'type' => 'meliscommerce/interface/meliscommerce_settings/interface/meliscommerce_settings_leftmenu'
                                     ],
